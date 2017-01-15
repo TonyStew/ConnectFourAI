d33d32ae780fb6339cb26a2653bc5bc4bf50b1bd
TonyStew <bino4444@gmail.com>
2017-01-14 22:30:36 -0800

    none

--- src/AI.java
+++ src/AI.java
@@ -23,6 +23,7 @@ public class AI {
         for(int i = -3; i <= 3; i++){
             if(height + i >= 0 && height + i <= 5 && col + i >= 0 && col + i <= 6) {
                 if (board[height + i][col + i] == 2) series++;
+                if (board[height + i][col + i] == 2) total++;
                 if (series == 4) total += series * 3;
                 if (board[height + i][col + i] == 1) series = 0;
             }
@@ -34,6 +35,7 @@ public class AI {
         for(int i = -3; i <= 3; i++){
             if(height - i >= 0 && height - i <= 5 && col + i >= 0 && col + i <= 6) {
                 if (board[height - i][col + i] == 2) series++;
+                if (board[height - i][col + i] == 2) total++;
                 if (series == 4) total += series * 3;
                 if (board[height - i][col + i] == 1) series = 0;
             }
@@ -45,6 +47,7 @@ public class AI {
         for(int i = -3; i <= 3; i++){
             if(height + i >= 0 && height + i <= 5) {
                 if (board[height + i][col] == 2) series++;
+                if (board[height + i][col] == 2) total++;
                 if (series == 4) total += series * 3;
                 if (board[height + i][col] == 1) series = 0;
             }
@@ -56,6 +59,7 @@ public class AI {
         for(int i = -3; i <= 3; i++){
             if(col + i >= 0 && col + i <= 6) {
                 if (board[height][col + i] == 2) series++;
+                if (board[height][col + i] == 2) total++;
                 if (series == 4) total += series * 3;
                 if (board[height][col + i] == 1) series = 0;
             }
