c3b3739a81811a659857d0a149360e0338c79a57
George Birch <birchgeorge8@gmail.com>
2017-01-15 06:11:54 -0800

    yes

--- src/AI2.java
+++ src/AI2.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by birch_000 on 1/15/2017.
  */
@@ -8,7 +11,29 @@ public class AI2 {
     }
 
     public int getMove(Board board) {
+        int[] solutions = new int[7];
+        for (int i = 0; i < 7; i++) {
+            Node root = new Node(board.getBoard(), 0);
+            root.board.place(i, false);
+            solutions[i] = firstSolution(root);
+        }
+        return choose(solutions);
+    }
 
+    private int firstSolution(Node root) {
+        boolean solutionFound = false;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node current = q.poll();
+            if (current.board.gameOver())
+                return current.depth % 2 == 0 ? current.depth : -current.depth;
+            for (int i = 0; i < 7; i++) {
+                Node next = new Node(current.board.getBoard(), current.depth + 1);
+                q.add(next);
+            }
+        }
+        return -420;
     }
 
     private int choose(int[] solutions) {
