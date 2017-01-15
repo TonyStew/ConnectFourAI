aa70eb4e350619306b381eb05ba9b220766a020f
TonyStew <bino4444@gmail.com>
2017-01-15 06:08:30 -0800

    none

--- src/AI2.java
+++ src/AI2.java
@@ -12,7 +12,22 @@ public class AI2 {
     }
 
     private int choose(int[] solutions) {
-
+        int choseIndex = 0;
+        boolean containsPositive = false;
+        for(int i: solutions){
+            if(i >= 0) containsPositive = true;
+        }
+        if(containsPositive) {
+            for (int i = 0; i < solutions.length; i++) {
+                if (solutions[i] < solutions[choseIndex] && solutions[i] >= 0) choseIndex = i;
+            }
+        }
+        else {
+            for(int i = 0; i < solutions.length; i++){
+                if(solutions[i] < solutions[choseIndex]) choseIndex = 1;
+            }
+        }
+        return choseIndex;
     }
 
 }
