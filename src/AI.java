430b2c5cb077e27c38437e6ed54b117c8710617b
George Birch <birchgeorge8@gmail.com>
2017-01-14 16:31:15 -0800

    some main work added AI class

---
+++ src/AI.java
@@ -0,0 +1,10 @@
+/**
+ * Created by birch_000 on 1/14/2017.
+ */
+public class AI {
+
+    public AI() {
+
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,58 @@
+/**
+ * Created by birch_000 on 1/14/2017.
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    private static Board board;
+    private static AI ai;
+
+    public static void main(String[] args) {
+        //initialization
+        board = new Board();
+        ai = new AI();
+        //run main loop
+        run();
+    }
+
+    public static void run() {
+        //ture = player's turn, false = ai's turn
+        boolean playersTurn = true;
+
+        //while (!Board.gameOver()) {
+        while (true) {
+            board.draw();
+            //player turn
+            if (playersTurn) {
+                boolean incorrectInput = false;
+                while (incorrectInput) {
+                    System.out.print("What row would you like to play in? (1-7): ");
+                    Scanner input = new Scanner(System.in);
+                    try {
+                        /*
+                        board.place(input.nextInt(), true);
+                        incorrectInput = false;
+                        */
+                        int answer = input.nextInt();
+                        input.nextLine();
+                        System.out.println(answer);
+                        incorrectInput = false;
+                    } catch (Exception e) {
+                        incorrectInput = true;
+                        System.out.println("Invalid input.");
+                    }
+                }
+                playersTurn = false;
+            }
+            //ai turn
+            else {
+                /*
+                board.place(ai.getMove(), false);
+                */
+                System.out.println("ai move");
+                playersTurn = true;
+            }
+        }
+    }
+}
