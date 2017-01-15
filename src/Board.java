38f943c5915de07b1077cc790e0029e95989abef
TonyStew <bino4444@gmail.com>
2017-01-14 16:34:21 -0800

    none

--- src/Board.java
+++ src/Board.java
@@ -2,32 +2,36 @@
  * Created by root on 1/14/17.
  */
 public class Board {
-    int[][] board = new int[6][7];
+    int[][] board = new int[6][7]; //row then column
     String blue = (char)27 + "[31m";
     String red = (char)27 + "[34m";
     String white = (char)27 + "[38m";
 
     public Board(){
-        setup();
+
     }
 
-    private void setup(){
-        for(int[] row: board){
-            for(int piece: row){
-                piece = 0;
+    public void place(int col, boolean player){
+        try {
+            for (int i = 0; i < 7; i++) {
+                if(player) {
+                    if (i == 6) board[6][col] = 1;
+                    else if (board[i][col] != 0) board[i - 1][col] = 1;
+                }
+                else{
+                    if (i == 6) board[6][col] = 2;
+                    else if (board[i][col] != 0) board[i - 1][col] = 2;
+                }
             }
         }
-    }
-
-    /*public boolean place(int cord, boolean player){
-        for(int[] row: board){
-
+        catch (Exception e){
+            System.out.println("you can't move there");
         }
     }
 
-    public boolean gameOver(){
+    //public boolean gameOver(){
 
-    } */
+    //}
 
     public void draw(){
         for(int[] row: board){
@@ -40,5 +44,6 @@ public class Board {
             }
             System.out.println();
         }
+        System.out.println(" 1   2   3   4   5   6   7");
     }
 }
