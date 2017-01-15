18eb2b98ea4e0927ab0e3154c2916ba8172a900e
George Birch <birchgeorge8@gmail.com>
2017-01-15 05:55:53 -0800

    yes

---
+++ src/AI2.java
@@ -0,0 +1,18 @@
+/**
+ * Created by birch_000 on 1/15/2017.
+ */
+public class AI2 {
+
+    public AI2() {
+
+    }
+
+    public int getMove(Board board) {
+
+    }
+
+    private int choose(int[] solutions) {
+
+    }
+
+}
